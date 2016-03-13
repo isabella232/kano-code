c89aedbc6fad8af34237796846b910902e7d90bf
Paul Varache <perso@paulvarache.ninja>
2016-03-13 19:58:38 +0000

    Add comment, better auth, sharing modal

--- app/scripts/service/modelManager.js
+++ app/scripts/service/modelManager.js
@@ -1,24 +1,24 @@
 // Private data - the state.
-let data = {}
+let data = {};
 
 // Model Manager for keeping the state of the app. Accessible through the following api.
 const ModelManager = {
 
-  // Get a piece of data.
-  get (name) {
-    return data[name]
-  },
+    // Get a piece of data.
+    get (name) {
+        return data[name];
+    },
 
-  // Set a piece of data providing the name and the info.
-  set (name, info) {
-    data[name] = info
-  },
+    // Set a piece of data providing the name and the info.
+    set (name, info) {
+        data[name] = info;
+    },
 
-  // Unset (remove) a piece of data.
-  unset (name) {
-    data[name] = ''
-  }
-}
+    // Unset (remove) a piece of data.
+    unset (name) {
+        data[name] = '';
+    }
+};
 
 // Expose the above api.
-export default ModelManager
+export default ModelManager;
