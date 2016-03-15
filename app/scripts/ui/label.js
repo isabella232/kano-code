071aa325e8483063e7ef47e1addaca9849f126a1
Andrei Constantinescu <andrei@kano.me>
2016-03-15 11:48:37 +0000

    Add text label and basic cutomisations.

---
+++ app/scripts/ui/label.js
@@ -0,0 +1,36 @@
+import UI from './ui';
+
+export default class Label extends UI {
+    constructor () {
+        super({
+            type: 'label',
+            label: 'The Label',
+            image: 'https://placeholdit.imgix.net/~text?txtsize=9&txt=100%C3%97100&w=100&h=100',
+            hue: 118,
+            customizable: {
+                style: ['background-color', 'color'],
+                properties: [{
+                    key: 'text',
+                    type: 'text',
+                    label: 'Text'
+                }]
+            }
+        });
+    }
+    addEventListener (name, callback) {
+        super.addEventListener.apply(this, arguments);
+        let element = this.getElement();
+        return element.addEventListener.apply(element, arguments);
+    }
+    removeListeners () {
+        let element = this.getElement();
+        this.listeners.forEach((listener) => {
+            element.removeEventListener.apply(element, listener);
+        });
+        super.removeListeners.apply(this, arguments);
+    }
+    stop () {
+        this.removeListeners();
+        super.stop.apply(this, arguments);
+    }
+}
