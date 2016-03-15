d30a1218f84b4dd59ee3a3a0462c64ebca382333
Andrei Constantinescu <andrei@kano.me>
2016-03-15 12:14:39 +0000

    Customize color and font size.

--- app/scripts/ui/label.js
+++ app/scripts/ui/label.js
@@ -8,7 +8,7 @@ export default class Label extends UI {
             image: 'https://placeholdit.imgix.net/~text?txtsize=9&txt=100%C3%97100&w=100&h=100',
             hue: 118,
             customizable: {
-                style: ['background-color', 'color'],
+                style: ['background-color', 'color', 'font-size'],
                 properties: [{
                     key: 'text',
                     type: 'text',
