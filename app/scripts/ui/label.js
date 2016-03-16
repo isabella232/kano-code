78d5feec0918694e687219af370046d507289af3
Andrei Constantinescu <andrei@kano.me>
2016-03-16 11:55:15 +0000

    Add blocks to set value of the label.

--- app/scripts/ui/label.js
+++ app/scripts/ui/label.js
@@ -16,5 +16,48 @@ export default class Label extends UI {
                 }]
             }
         });
+
+        this.addBlock({
+            id: 'label_text_set_value',
+            output: false,
+            message0: 'set label value to %1',
+            args0: [{
+                type: "input_value",
+                name: "INPUT"
+            }],
+
+            javascript: (hw) => {
+                return function (block) {
+                    let value = Blockly.JavaScript.valueToCode(block, 'INPUT');
+
+                    return [`devices.get('${hw.id}').setValue(${value});`];
+                };
+            },
+            natural: (hw) => {
+                return function (block) {
+                    return [`input set value`];
+                };
+            }
+        });
+    }
+
+
+    setValue (value) {
+        this.getElement().setValue(value);
+    }
+
+    addEventListener () {
+        let element;
+        super.addEventListener.apply(this, arguments);
+        element = this.getElement();
+        return element.addEventListener.apply(element, arguments);
+    }
+
+    removeListeners () {
+        let element = this.getElement();
+        this.listeners.forEach((listener) => {
+            element.removeEventListener.apply(element, listener);
+        });
+        super.removeListeners();
     }
 }
