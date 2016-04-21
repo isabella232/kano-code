a9978f602e3656ee369f3f96b3a3d6b2f6402c83
Paul Varache <perso@paulvarache.ninja>
2016-04-21 16:40:46 +0100

    Rename Frame to Box

--- app/scripts/part/index.js
+++ app/scripts/part/index.js
@@ -2,7 +2,7 @@ import Part from './part';
 import UI from './ui';
 import Data from './data';
 import Button from './button';
-import Frame from './frame';
+import Box from './box';
 import TextInput from './text-input';
 import Text from './text';
 import Map from './map';
@@ -14,7 +14,7 @@ import Image from './image';
 let part,
     partTypes;
 
-export default part = [Button, Frame, TextInput, Text, Map, ISS, Weather, Share, Image];
+export default part = [Button, Box, TextInput, Text, Map, ISS, Weather, Share, Image];
 
 
 partTypes = {
