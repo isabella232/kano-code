26614b4dec2b2214716f88b8f8aea25267283231
Radek Pazdera <radek@kano.me>
2016-04-25 18:42:11 +0100

    Adding share flow

--- app/elements/kano-app-editor/kano-app-editor.js
+++ app/elements/kano-app-editor/kano-app-editor.js
@@ -157,28 +157,6 @@ class KanoAppEditor {
         }
     }
     /**
-     * Opens the sharing modal and share the app
-     */
-    share () {
-        let modal = this.$['share-modal'],
-            image_generator = this.$['image_generator'],
-            image,
-            workspace_info;
-
-        image = image_generator.getImage();
-        workspace_info = JSON.stringify(this.save());
-        this.fire('data-generated', {image, workspace_info});
-        modal.open();
-    }
-    confirmShare (e) {
-        this.fire('share', e.detail);
-        this.dismissShare();
-    }
-    dismissShare () {
-        let modal = this.$['share-modal'];
-        modal.close();
-    }
-    /**
      * Save the current work in the local storage
      * TODO trigger that every 5sec or so if there is any changes
      */
@@ -199,6 +177,12 @@ class KanoAppEditor {
         }
 
         return savedApp;
+    }
+    share () {
+        let image_generator = this.$['image_generator'];
+
+        this.fire('share', {cover: image_generator.getImage(),
+                            workspaceInfo: JSON.stringify(this.save())});
     }
     /**
      * Load the saved work from the local storage
