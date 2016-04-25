679b53a4f97076cb1494db2689785e939287979e
Paul Varache <perso@paulvarache.ninja>
2016-04-25 11:00:19 +0100

    Add background blocks

---
+++ app/scripts/blockly/background.js
@@ -0,0 +1,44 @@
+const COLOUR = '#C24242';
+
+let register = (Blockly) => {
+    Blockly.Blocks.set_background_color = {
+        init: function () {
+            let json = {
+                id: 'loop_forever',
+                colour: COLOUR,
+                message0: 'Set background color to %1',
+                args0: [{
+                    type: "input_value",
+                    name: "COLOUR",
+                    check: "Colour"
+                }],
+                previousStatement: null,
+                nextStatement: null
+            };
+            this.jsonInit(json);
+        }
+    };
+
+    Blockly.JavaScript.set_background_color = (block) => {
+        let colour = Blockly.JavaScript.valueToCode(block, 'COLOUR');
+        return `devices.get('dropzone').style.backgroundColor = ${colour};\n`;
+    };
+
+    Blockly.Pseudo.set_background_color = (block) => {
+        let colour = Blockly.Pseudo.valueToCode(block, 'COLOUR');
+        return `setBackgroundColour(${colour});\n`;
+    };
+};
+
+let category = {
+    name: 'Background',
+    colour: COLOUR,
+    blocks: [{
+        id: 'set_background_color'
+    }]
+};
+
+export default {
+    register,
+    category
+};
