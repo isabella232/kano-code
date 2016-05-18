9d7bcf0960b8b54d3d592865fd6244b59dfe65f4
Paul Varache <perso@paulvarache.ninja>
2016-05-18 15:58:45 +0100

    Add gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -390,5 +390,19 @@ gulp.task('copy-test', () => {
         .pipe(gulp.dest('www/test'));
 });
 
+gulp.task('doc', ['copy-doc'], () => {
+    gulp.src('app/doc/**/*')
+        .pipe($.if('index.html', $.htmlReplace({ doc: `
+            <script>window.ELEMENTS_LOCATION = './elements/';</script>
+        ` })))
+        .pipe($.if('*.html', $.replace(/(href|src)="\.\.\/bower_components(.+)\.html"/g, '$1="./bower_components$2.html"')))
+        .pipe(gulp.dest('www-doc'));
+});
+
+gulp.task('copy-doc', () => {
+    gulp.src(['app/elements/**/*', 'app/bower_components/**/*', 'app/assets/**/*'], { base: 'app/' })
+        .pipe(gulp.dest('www-doc'));
+});
+
 gulp.task('dev', ['watch', 'serve']);
 gulp.task('build-dev', ['sass-dev', 'bundle-dev', 'elements-dev', 'assets-dev', 'views-dev', 'copy-dev']);
