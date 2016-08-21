59a2bee52d200c73ce921f49aca26ab46ee800e5
Paul Varache <perso@paulvarache.ninja>
2016-08-21 13:32:05 +0100

    Add sound player

--- app/scripts/app-modules/speaker.js
+++ app/scripts/app-modules/speaker.js
@@ -3,8 +3,30 @@ import TextToSpeech from './service/text-to-speech';
 let speaker;
 
 export default speaker = {
+    sources: [],
+    samples: {},
     say (text, rate, language) {
         speaker.tts.speak(text, rate, language);
+    },
+    createSource (buffer) {
+        let source = speaker.ctx.createBufferSource();
+        source.buffer = buffer;
+        source.connect(speaker.ctx.destination);
+        speaker.sources.push(source);
+        return source;
+    },
+    loadSample (name) {
+        if (speaker.samples[name]) {
+            return Promise.resolve(speaker.samples[name]);
+        } else {
+            return fetch(`/assets/audio/samples/${name}.wav`)
+                .then(res => res.arrayBuffer())
+                .then(data => {
+                    let buffer = speaker.ctx.decodeAudioData(data);
+                    speaker.samples[name] = buffer;
+                    return buffer;
+                });
+        }
     },
     methods: {
         say (text, rate, language) {
@@ -14,17 +36,39 @@ export default speaker = {
                 });
             }
             speaker.say(text, rate, language);
+        },
+        play (sample) {
+            speaker.loadSample(sample)
+                .then(buffer => {
+                    let source = speaker.createSource(buffer);
+                    source.start(speaker.ctx.currentTime);
+                });
+        },
+        loop (sample) {
+            speaker.loadSample(sample)
+                .then(buffer => {
+                    let source = speaker.createSource(buffer);
+                    source.loop = true;
+                    source.start(speaker.ctx.currentTime);
+                });
+        },
+        stop () {
+            speaker.lifecycle.stop();
         }
     },
     lifecycle: {
         stop () {
-            if (!speaker.tts) {
-                return;
+            if (speaker.tts) {
+                speaker.tts.stop();
             }
-            speaker.tts.stop();
+            speaker.sources.forEach(source => source.stop());
+            speaker.sources = [];
         }
     },
     config (opts) {
+        window.AudioContext = window.AudioContext || window.webkitAudioContext;
         speaker.tts = TextToSpeech(opts);
+        speaker.ctx = new AudioContext();
+        console.log(speaker);
     }
 };
--- app/scripts/parts/speaker.js
+++ app/scripts/parts/speaker.js
@@ -2,12 +2,22 @@
 
 let speaker;
 
+const samples = {
+    kano: {
+        'level-up': 'Level up'
+    },
+    loops: {
+        'amen': 'Amen'
+    }
+};
+const COLOUR = '#FFB347';
+
 export default speaker = {
     partType: 'hardware',
     type: 'speaker',
     label: 'Speaker',
     image: '/assets/part/speaker.svg',
-    colour: '#FFB347',
+    colour: COLOUR,
     blocks: [{
         block: () => {
             return {
@@ -69,6 +79,144 @@ export default speaker = {
                     lang = block.getFieldValue('LANGUAGE'),
                     code = `speaker.say(${text}, ${rate}, "${lang}");\n`;
                 return code;
+            };
+        }
+    },{
+        block: () => {
+            return {
+                id: 'speaker_play',
+                message0: 'Speaker: play %1',
+                args0: [{
+                    type: "input_value",
+                    name: "SAMPLE",
+                    check: 'Sample'
+                }],
+                previousStatement: null,
+                nextStatement: null
+            };
+        },
+        javascript: () => {
+            return (block) => {
+                let sample = Blockly.JavaScript.valueToCode(block, 'SAMPLE') || {},
+                    code = `speaker.play(${sample});\n`;
+                return code;
+            };
+        },
+        pseudo: () => {
+            return (block) => {
+                let sample = Blockly.Pseudo.valueToCode(block, 'SAMPLE') || {},
+                    code = `speaker.play(${sample});\n`;
+                return code;
+            };
+        }
+    },{
+        block: () => {
+            return {
+                id: 'speaker_loop',
+                message0: 'Speaker: loop %1',
+                args0: [{
+                    type: "input_value",
+                    name: "SAMPLE",
+                    check: 'Sample'
+                }],
+                previousStatement: null,
+                nextStatement: null
+            };
+        },
+        javascript: () => {
+            return (block) => {
+                let sample = Blockly.JavaScript.valueToCode(block, 'SAMPLE') || {},
+                    code = `speaker.loop(${sample});\n`;
+                return code;
+            };
+        },
+        pseudo: () => {
+            return (block) => {
+                let sample = Blockly.Pseudo.valueToCode(block, 'SAMPLE') || {},
+                    code = `speaker.loop(${sample});\n`;
+                return code;
+            };
+        }
+    },{
+        block: () => {
+            return {
+                id: 'speaker_stop',
+                message0: 'Speaker: stop',
+                previousStatement: null,
+                nextStatement: null
+            };
+        },
+        javascript: () => {
+            return (block) => {
+                let code = `speaker.stop();\n`;
+                return code;
+            };
+        },
+        pseudo: () => {
+            return (block) => {
+                let code = `speaker.stop();\n`;
+                return code;
+            };
+        }
+    },{
+        block: (part) => {
+            let sampleSet = Object.keys(samples),
+                id = 'speaker_sample';
+            Blockly.Blocks[`${part.id}#${id}`] = {
+                init: function () {
+
+                    let setDropdown = new Blockly.FieldDropdown(sampleSet.map(name => [name, name]), function (option) {
+                        this.sourceBlock_.updateShape_(option);
+                    });
+
+                    this.setColour(part.colour);
+
+                    this.appendDummyInput()
+                        .appendField(setDropdown, 'SET');
+
+                    this.setOutput('Sample');
+
+                    this.setInputsInline(true);
+
+                    this.createInputs_('kano');
+                },
+                updateShape_: function (option) {
+                    this.removeInput('SAMPLE');
+                    this.createInputs_(option);
+                },
+                createInputs_: function (option) {
+                    let options = Object.keys(samples[option]).map(key => [samples[option][key], key]),
+                        dropdown = new Blockly.FieldDropdown(options);
+                    this.appendDummyInput('SAMPLE')
+                        .appendField(dropdown, 'SAMPLE');
+                },
+                domToMutation: function (xmlElement) {
+                    let type = xmlElement.getAttribute('set');
+                    this.updateShape_(type);
+                },
+                mutationToDom: function () {
+                    let container = document.createElement('mutation'),
+                        type = this.getFieldValue('SET');
+                    container.setAttribute('set', type);
+                    return container;
+                }
+            };
+            return {
+                id,
+                colour: COLOUR,
+                doNotRegister: true
+            }
+        },
+        javascript: () => {
+            return (block) => {
+                let sample = block.getFieldValue('SAMPLE') || 'amen';
+                return [`'${sample}'`];
+            };
+        },
+        pseudo: () => {
+            return (block) => {
+                let sample = block.getFieldValue('SAMPLE') || 'amen';
+                return [`'${sample}'`];
             };
         }
     }]
