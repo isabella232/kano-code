4734892142461ecdc4d0c433a5b52fae19c5da9d
Paul Varache <perso@paulvarache.ninja>
2016-09-01 18:17:31 +0100

    Add webcam when not connected to the kit

--- app/scripts/app-modules/camera.js
+++ app/scripts/app-modules/camera.js
@@ -5,9 +5,12 @@ let camera;
 export default camera = {
     methods: {
         connect (info) {
-            HardwareAPI.connectToSocket();
-            HardwareAPI.socket.on('connect', () => {
-                HardwareAPI.socket.emit('camera:init', info);
+            return new Promise((resolve, reject) => {
+                HardwareAPI.connectToSocket();
+                HardwareAPI.socket.on('connect', () => {
+                    HardwareAPI.socket.emit('camera:init', info);
+                    return resolve();
+                });
             });
         },
         flash (color, length) {
