ddf06a0d9bb30c43a35febd2d261be5a884c0414
Paul Varache <perso@paulvarache.ninja>
2016-09-02 14:25:41 +0100

    path WIP

---
+++ app/scripts/kano/make-apps/msg/en.js
@@ -0,0 +1,32 @@
+(function (Kano) {
+    Kano.MakeApps = Kano.MakeApps || {};
+
+    Kano.MakeApps.Msg = Kano.MakeApps.Msg || {};
+
+    Kano.MakeApps.Msg['add-parts-1'] = "We'll start by adding parts to our app.";
+    Kano.MakeApps.Msg['add-parts-2'] = "To build the interface of the app, we need parts";
+
+    Kano.MakeApps.Msg['open-parts-drawer-1'] = "Click here to see all the parts.";
+    Kano.MakeApps.Msg['open-parts-drawer-2'] = "Open the parts drawer by clicking here.";
+
+    Kano.MakeApps.Msg['start-code-1'] = "Now we can start coding.";
+    Kano.MakeApps.Msg['start-code-2'] = "All the parts are here, let's code!";
+
+    Kano.MakeApps.Msg['close-parts-drawer-1'] = "Close the drawer.";
+    Kano.MakeApps.Msg['close-parts-drawer-2'] = "Hit that button";
+
+    Kano.MakeApps.Msg['connect-blocks-1'] = "Connect it to this block";
+
+    Kano.MakeApps.Msg['drop-codespace-1'] = "Drop the block anywhere on your code space";
+
+
+    Kano.MakeApps.Msg.Groups = Kano.MakeApps.Msg.Groups || {};
+
+    Kano.MakeApps.Msg.Groups['add-parts'] = ['add-parts-1', 'add-parts-2'];
+    Kano.MakeApps.Msg.Groups['open-parts-drawer'] = ['open-parts-drawer-1', 'open-parts-drawer-2'];
+    Kano.MakeApps.Msg.Groups['start-code'] = ['start-code-1', 'start-code-2'];
+    Kano.MakeApps.Msg.Groups['close-parts-drawer'] = ['close-parts-drawer-1', 'close-parts-drawer-2'];
+    Kano.MakeApps.Msg.Groups['connect-blocks'] = ['connect-blocks-1'];
+    Kano.MakeApps.Msg.Groups['drop-codespace'] = ['drop-codespace-1'];
+
+})(window.Kano = window.Kano || {});
