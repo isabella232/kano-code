c23bd2b8cf9f5587dd90a8fa7e06a2309dd4554e
Paul Varache <perso@paulvarache.ninja>
2016-09-06 16:35:42 +0100

    Add filters WIP

--- app/scripts/mode/common/filters.js
+++ app/scripts/mode/common/filters.js
@@ -100,4 +100,93 @@ export default [{
             return `devices.get('${part.id}').addFilter('colorize', ${color}, ${value});\n`;
         };
     }
+},{
+    block: () => {
+        return {
+                id: 'filter_threshold',
+                message0: 'frame: black and white',
+                message1: 'threshold %1',
+                args1: [{
+                    type: 'input_value',
+                    name: 'THRESHOLD',
+                    check: 'Number',
+                    align: 'RIGHT'
+                }],
+                previousStatement: null,
+                nextStatement: null
+            };
+    },
+    javascript: (part) => {
+        return (block) => {
+            let threshold = Blockly.JavaScript.valueToCode(block, 'THRESHOLD') || 50;
+            threshold = Math.max(0, Math.min(255, threshold * 2.55));
+            return `devices.get('${part.id}').addFilter('threshold', ${threshold});\n`;
+        };
+    },
+    pseudo: (part) => {
+        return (block) => {
+            let threshold = Blockly.Pseudo.valueToCode(block, 'THRESHOLD') || 50;
+            threshold = Math.max(0, Math.min(255, threshold * 2.55));
+            return `devices.get('${part.id}').addFilter('threshold', ${threshold});\n`;
+        };
+    }
+},{
+    block: () => {
+        return {
+                id: 'filter_pixelate',
+                message0: 'frame: pixelate',
+                message1: 'size %1',
+                args1: [{
+                    type: 'input_value',
+                    name: 'SIZE',
+                    check: 'Number',
+                    align: 'RIGHT'
+                }],
+                previousStatement: null,
+                nextStatement: null
+            };
+    },
+    javascript: (part) => {
+        return (block) => {
+            let size = Blockly.JavaScript.valueToCode(block, 'SIZE') || 20;
+            size = Math.max(1, Math.min(100, size));
+            return `devices.get('${part.id}').addFilter('pixelate', ${size});\n`;
+        };
+    },
+    pseudo: (part) => {
+        return (block) => {
+            let size = Blockly.Pseudo.valueToCode(block, 'SIZE') || 20;
+            size = Math.max(1, Math.min(100, size));
+            return `devices.get('${part.id}').addFilter('pixelate', ${size});\n`;
+        };
+    }
+},{
+    block: () => {
+        return {
+                id: 'filter_contrast',
+                message0: 'frame: contrast %1',
+                args0: [{
+                    type: 'input_value',
+                    name: 'CONTRAST',
+                    check: 'Number',
+                    align: 'RIGHT'
+                }],
+                previousStatement: null,
+                nextStatement: null
+            };
+    },
+    javascript: (part) => {
+        return (block) => {
+            let contrast = Blockly.JavaScript.valueToCode(block, 'CONTRAST') || 50;
+            contrast = Math.max(-150, Math.min(150, (contrast * 3) - 150));
+            return `devices.get('${part.id}').addFilter('contrast', ${contrast});\n`;
+        };
+    },
+    pseudo: (part) => {
+        return (block) => {
+            let contrast = Blockly.Pseudo.valueToCode(block, 'CONTRAST') || 50;
+            contrast = Math.max(-100, Math.min(100, (contrast * 2) - 100));
+            return `devices.get('${part.id}').addFilter('contrast', ${contrast});\n`;
+        };
+    }
 }];
