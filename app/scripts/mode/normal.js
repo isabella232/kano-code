da025cc873927daafe210fa532941d7005ae3235
Paul Varache <perso@paulvarache.ninja>
2016-10-07 10:47:35 +0100

    Add transparency to draw

--- app/scripts/mode/normal.js
+++ app/scripts/mode/normal.js
@@ -30,6 +30,37 @@ blocks.push({
     }
 });
 
+blocks.push({
+    block: (ui) => {
+        return {
+            id: 'set_transparency',
+            message0: `${ui.name}: set transparency to %1`,
+            args0: [{
+                type: 'input_value',
+                name: 'ALPHA',
+                check: 'Number'
+            }],
+            previousStatement: null,
+            nextStatement: null,
+            shadow: {
+                'ALPHA': '<shadow type="math_number"><field name="NUM">100</field></shadow>'
+            }
+        };
+    },
+    javascript: (ui) => {
+        return function (block) {
+            let alpha = Blockly.JavaScript.valueToCode(block, 'ALPHA');
+            return `devices.get('${ui.id}').setTransparency(${alpha});`;
+        };
+    },
+    pseudo: (ui) => {
+        return function (block) {
+            let alpha = Blockly.JavaScript.valueToCode(block, 'ALPHA');
+            return `devices.get('${ui.id}').setTransparency(${alpha});`;
+        };
+    }
+});
+
 blocks = blocks.concat(setters);
 blocks = blocks.concat(space);
 blocks = blocks.concat(paths);
