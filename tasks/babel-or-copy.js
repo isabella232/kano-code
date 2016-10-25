23771a42cf18a66029cb8006165d2dc141047119
Gabriel G <gpgabriel@users.noreply.github.com>
2016-10-25 19:21:24 +0100

    Fix gulp parts-api. (#597)

--- tasks/babel-or-copy.js
+++ tasks/babel-or-copy.js
@@ -1,16 +1,24 @@
 'use strict';
 module.exports = (gulp, $) => {
-    /**
-     * Skip babel if the target env is ES6 capable
-     */
-    function babelOrCopy(src, opts) {
-        let stream = gulp.src(src, opts);
-        if (!process.env.ES6) {
-            stream = stream
-                .pipe($.if('*.html', $.crisper({ scriptInHead: false })))
-                .pipe($.if('*.js', $.babel({ presets: ['es2015'], sourceMaps: $.debug ? 'inline' : false })));
-        }
-        return stream;
-    }
-    return babelOrCopy;
+
+   function hasExt(ext) {
+       return (file) => {
+           return file.relative.split('.').pop() === ext;
+       }
+   }
+
+   /**
+    * Skip babel if the target env is ES6 capable
+    */
+   function babelOrCopy(src, opts) {
+       let stream = gulp.src(src, opts);
+       if (!process.env.ES6) {
+           stream = stream
+               .pipe($.vulcanize())
+               .pipe($.if(hasExt('html'), $.crisper({ scriptInHead: false })))
+               .pipe($.if(hasExt('js'), $.babel({ presets: ['es2015'], sourceMaps: $.debug ? 'inline' : false })));
+       }
+       return stream;
+   }
+   return babelOrCopy;
 };
