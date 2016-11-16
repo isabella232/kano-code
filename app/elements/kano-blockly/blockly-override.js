613afff8f722bb0c1b9f7ac54d00d69440a323f3
Paul Varache <perso@paulvarache.ninja>
2016-11-16 14:49:33 +0000

    Update design

--- app/elements/kano-blockly/blockly-override.js
+++ app/elements/kano-blockly/blockly-override.js
@@ -594,6 +594,40 @@ Blockly.BlockSvg.prototype.renderDrawLeft_ =
       return this.svgGroup_;
     };
 
+    /**
+     * Move the zoom controls to the bottom-right corner.
+     */
+    Blockly.ZoomControls.prototype.position = function() {
+        var metrics = this.workspace_.getMetrics();
+        if (!metrics) {
+            // There are no metrics available (workspace is probably not visible).
+            return;
+        }
+        if (this.workspace_.RTL) {
+            this.left_ = this.MARGIN_SIDE_ + Blockly.Scrollbar.scrollbarThickness;
+            if (metrics.toolboxPosition == Blockly.TOOLBOX_AT_LEFT) {
+            this.left_ += metrics.flyoutWidth;
+            if (this.workspace_.toolbox_) {
+                this.left_ += metrics.absoluteLeft;
+            }
+            }
+        } else {
+            this.left_ = metrics.viewWidth + metrics.absoluteLeft -
+                this.WIDTH_ - this.MARGIN_SIDE_ - Blockly.Scrollbar.scrollbarThickness;
+
+            if (metrics.toolboxPosition == Blockly.TOOLBOX_AT_RIGHT) {
+            this.left_ -= metrics.flyoutWidth;
+            }
+        }
+        this.top_ = metrics.viewHeight + metrics.absoluteTop -
+            this.HEIGHT_ - this.bottom_;
+        if (metrics.toolboxPosition == Blockly.TOOLBOX_AT_BOTTOM) {
+            this.top_ -= metrics.flyoutHeight;
+        }
+        this.svgGroup_.setAttribute('transform',
+            'translate(' + this.left_ + ',' + this.top_ + ')');
+    };
+
 
 
     /**
