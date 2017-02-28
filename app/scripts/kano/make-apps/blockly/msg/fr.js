7b61cf391455c2ce58091bba82ee235e1fd9067d
Brandon Jackson <brandonscottjackson@gmail.com>
2017-02-28 10:30:23 +0000

    i18n for math + variables blockly categories * also fixed typo: VARIALBLES -> VARIABLES

--- app/scripts/kano/make-apps/blockly/msg/fr.js
+++ app/scripts/kano/make-apps/blockly/msg/fr.js
@@ -7,7 +7,7 @@
     Blockly.Msg.CATEGORY_LISTS = "Listes";
     Blockly.Msg.CATEGORY_LOGIC = "Logique";
     Blockly.Msg.CATEGORY_MATH = "Math";
-    Blockly.Msg.CATEGORY_VARIALBLES = "Variables";
+    Blockly.Msg.CATEGORY_VARIABLES = "Variables";
     Blockly.Msg.CATEGORY_FUN = "Fun";
     Blockly.Msg.CATEGORY_MISC = "Autres";
 
