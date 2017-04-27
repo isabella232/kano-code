88f2d264f800149633740b7de0ed8f94b3f598c1
Gabor Gabriel <gabrielgabor@live.com>
2017-04-27 13:49:41 +0100

    Refactor sendToKit and parts-api gulp task.

--- tasks/parts-api.js
+++ tasks/parts-api.js
@@ -1,14 +1,18 @@
 "use strict";
+
+var rename = require('gulp-rename');
+
 module.exports = (gulp, $) => {
 
     /**
      * No need to babelify here, this code will be sent to a node environment
      */
     gulp.task('parts-api', () => {
-        let stream = gulp.src('app/scripts/kano/make-apps/parts-api/parts-api.html', { base: 'app' })
+        let stream = gulp.src('app/scripts/kano/make-apps/parts-api/index.html', { base: 'app' })
             .pipe($.vulcanize())
             .pipe($.if('*.html', $.crisper({ scriptInHead: false })))
             .pipe($.if('*.js', $.transpile()))
+            .pipe(rename('app/scripts/kano-code-lib.js'))
             .pipe($.if('*.js', gulp.dest('www')));
         return stream;
     });
