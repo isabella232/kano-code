af1d9947eed73d92ebb8bc770c6c81b6e308116a
Viktor Dorozsmai <vikdoro@gmail.com>
2017-05-10 15:48:16 +0100

    Create random sound block

--- app/scripts/kano/make-apps/blockly/msg/en.js
+++ app/scripts/kano/make-apps/blockly/msg/en.js
@@ -112,6 +112,8 @@
     Blockly.Msg.BLOCK_SPEAKER_ITALIAN = "Italian";
     Blockly.Msg.BLOCK_SPEAKER_PLAY = "play %1";
     Blockly.Msg.BLOCK_SPEAKER_LOOP = "loop %1";
+    Blockly.Msg.BLOCK_SPEAKER_RANDOM = "random sound";
+    Blockly.Msg.BLOCK_SPEAKER_RANDOM_FROM = "random %1";
 
     Blockly.Msg.BLOCK_STICKER_SET = "set to %1";
     Blockly.Msg.BLOCK_STICKER_RANDOM = "random sticker";
