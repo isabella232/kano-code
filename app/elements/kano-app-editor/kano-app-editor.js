54dbb02209d5660c0110a116abcf2d1241d45859
Radek Pazdera <radek@kano.me>
2017-07-20 11:21:25 +0100

    Load parts with the correct colours again

--- app/elements/kano-app-editor/kano-app-editor.js
+++ app/elements/kano-app-editor/kano-app-editor.js
@@ -432,7 +432,7 @@ Polymer({
         this.set('addedParts', addedParts);
         // Force a color update and a register block to make sure the loaded code will be
         // rendered with the right colors
-        this.updateColors();
+        Kano.MakeApps.Utils.updatePartsColors(this.addedParts);
         this.$['root-view'].computeBlocks();
         this.set('code', savedApp.code);
         this.set('background', savedApp.background);
