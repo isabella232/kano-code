1141f94ee5b31382bfff59e2ba96ea605a99cd7e
Paul McKenna <paulmck21@gmail.com>
2018-07-31 11:47:15 +0100

    removed auto formatting

--- app/profiles/normal/editor.js
+++ app/profiles/normal/editor.js
@@ -1,26 +1,13 @@
 import * as code from '../../lib/index.js';
-import {
-    DrawToolbox
-} from '../../lib/meta-api/modules/draw.js';
-import {
-    KanoCodeWorkspaceViewProvider
-} from '../../scripts/workspace/index.js';
+import { DrawToolbox } from '../../lib/meta-api/modules/draw.js';
+import { KanoCodeWorkspaceViewProvider } from '../../scripts/workspace/index.js';
 
-import {
-    PartsPlugin
-} from '../../lib/parts/index.js';
+import { PartsPlugin } from '../../lib/parts/index.js';
 
-import {
-    DrawOutputProfile
-} from './output.js';
+import { DrawOutputProfile } from './output.js';
 
-import {
-    AllApis,
-    EventsModuleFactory
-} from '../../scripts/meta-api/all.js';
-import {
-    BackgroundEditorPlugin
-} from '../background/index.js';
+import { AllApis, EventsModuleFactory } from '../../scripts/meta-api/all.js';
+import { BackgroundEditorPlugin } from '../background/index.js';
 
 const COLOR = '#82C23D';
 
@@ -83,103 +70,4 @@ export class DrawEditorProfile extends code.EditorProfile {
 }
 
 export default DrawEditorProfile;
-        shadow: block.shadow,
-    };
-});
-const category = {
-    name: 'Draw',
-    id: 'draw',
-    colour: COLOR,
-    blocks: categoryBlocks,
-};
 
-const DrawToolbox = {
-    type: 'blockly',
-    id: 'draw',
-    typeScriptDefinition: `
-        declare namespace draw {}
-    `,
-    register(Blockly) {
-        const definitions = [];
-        blocks.forEach((definition) => {
-            if (typeof definition === 'object') {
-                definitions.push(definition);
-            }
-        });
-        definitions.forEach((definition) => {
-            const block = definition.block(DrawToolbox.category);
-            block.colour = COLOR;
-            if (!block.doNotRegister) {
-                Blockly.Blocks[block.id] = {
-                    init() {
-                        this.jsonInit(block);
-                    },
-                };
-                Blockly.Blocks[block.id].customColor = block.colour;
-            }
-            Blockly.JavaScript[block.id] = definition.javascript(DrawToolbox.category);
-        });
-    },
-    category,
-    defaults: {},
-};
-
-class WorkspaceFramePlugin extends code.Plugin {
-    onInstall(editor) {
-        this.editor = editor;
-    }
-    onInject() {
-        this.editor.output.on('running-state-changed', () => {
-            this.editor.workspaceView.root.running = this.editor.output.getRunningState();
-        });
-        this.editor.workspaceView.root.addEventListener('reset-app-state', () => {
-            this.editor.output.restart();
-        });
-    }
-}
-
-class DefaultSourcePlugin extends code.Plugin {
-    onInstall(editor) {
-        this.editor = editor;
-    }
-    onImport(data) {
-        if (!data || !data.source) {
-            data.source = '<xml xmlns="http://www.w3.org/1999/xhtml"><variables></variables><block type="global_when" id="default_part_event_id" x="62" y="120"><field name="EVENT">global.start</field></block></xml>';
-        }
-    }
-}
-
-export class DrawEditorProfile extends code.EditorProfile {
-    constructor(editor) {
-        super();
-        this.editor = editor;
-        this._outputProfile = new DrawOutputProfile();
-        this.eventsToolbox = EventsModuleFactory(this.editor);
-        this._toolbox = AllApis.concat(DrawToolbox);
-        this._toolbox.unshift(this.eventsToolbox);
-    }
-    get id() { return 'draw'; }
-    get workspaceViewProvider() {
-        const workspaceViewProvider = new KanoCodeWorkspaceViewProvider(
-            'kano-editor-normal',
-            {
-                width: 512,
-                height: 384,
-            },
-        );
-        return workspaceViewProvider;
-    }
-    get outputProfile() {
-        return this._outputProfile;
-    }
-    get plugins() {
-        return [new PartsPlugin(this.outputProfile.partsPlugin), new BackgroundEditorPlugin(this.outputProfile.backgroundPlugin), new WorkspaceFramePlugin(), new DefaultSourcePlugin()];
-    }
-    get toolbox() {
-        return this._toolbox;
-    }
-}
-
-export default DrawEditorProfile;
-
-export { DrawToolbox };
