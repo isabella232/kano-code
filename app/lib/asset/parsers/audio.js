ca249104f4b5ab77d69f6fe8f04fd7bf75b2c661
Paul McKenna <paulmck21@gmail.com>
2018-09-17 16:59:20 +0100

    better ios audio fix

--- app/lib/asset/parsers/audio.js
+++ app/lib/asset/parsers/audio.js
@@ -4,14 +4,6 @@ export default {
     test: /\.wav|\.m4a|\.mp3$/,
     parser: (r) => {
         return r.arrayBuffer()
-            .then(data => new Promise((resolve, reject) => {
-                if ('webkitAudioContext' in window) {
-                    const source = AudioPlayer.context.createBufferSource();
-                    source.buffer = AudioPlayer.context.createBuffer(data, false);
-                    resolve(source.buffer);
-                } else {
-                    AudioPlayer.context.decodeAudioData(data, buffer => resolve(buffer));
-                }
-            }));
+            .then(data => AudioPlayer.context.decodeAudioData(data));
     },
 };
