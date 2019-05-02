6a35c52562234deaf8e94e7275edb16871bbf6a3
Paul McKenna <paulmck21@gmail.com>
2019-05-02 10:50:42 +0100

    background colour fix

--- src/app/lib/creation/creation-preview-provider.ts
+++ src/app/lib/creation/creation-preview-provider.ts
@@ -36,6 +36,21 @@ export class CreationImagePreviewProvider extends CreationCustomPreviewProvider 
         canvas.height = this.size.height;
         // Use the output from the editor, not the player
         const res = this.editor.output.render(canvas.getContext('2d'));
+        // TODO
+        // The background of the canvas is set in CSS and when we make an image from canvas
+        // it does not render with the background colour.
+        // To add the background colour into the canvas use something like this:
+        
+        // let width = this.session.width * this.session.ratio,
+        //     height = this.session.height * this.session.ratio;
+        // const bgColor = this.session.settings.bg;
+        // this.session.ctx.globalCompositeOperation = 'destination-over';
+        // this.session.ctx.fillStyle = this.session.settings.bg;
+        // this.session.ctx.beginPath();
+        // this.session.ctx.rect(0, 0, width, height);
+        // this.session.ctx.closePath();
+        // this.session.ctx.fill();
+
         let p = res;
         if (!(res instanceof Promise)) {
             p = Promise.resolve(res);
