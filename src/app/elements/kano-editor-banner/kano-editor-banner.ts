377d1178cedefe70ab497ff431a85ec2bb54debf
Gi <gise11e@me.com>
2019-06-04 14:39:52 +0100

    SX-812 challenge generator banner (#1660)
    * v3.0.0-rc.5
    
    * widget WIP
    
    * banner styling
    
    * styling
    
    * replaced rgb for hex

--- src/app/elements/kano-editor-banner/kano-editor-banner.ts
+++ src/app/elements/kano-editor-banner/kano-editor-banner.ts
@@ -20,57 +20,43 @@ export class KCEditorBanner extends LitElement {
 
     static get styles() {
         return [css`
-            @keyframes pop-in {
-                0% {
-                    transform: scale(1, 1);
-                }
-                50% {
-                    transform: scale(1.1, 1.1);
-                }
-                100% {
-                    transform: scale(1.0, 1.0);
-                }
-            }
+        
             :host {
                 position: relative;
                 display: flex;
-                flex-direction: row;
-                align-items: stretch;
-                padding: 16px;
-                background: white;
+                flex-direction: column;
+                background: #FFFF;
                 box-sizing: border-box;
                 border-radius: 6px;
                 box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.15);
             }
-            :host(.animate) {
-                animation: pop-in 200ms linear 1;
-            }
-            kc-circle-progress,
-            slot[name="progress"]::slotted(*) {
+
+            slot[name="avatar"]::slotted(*) {
                 width: 56px;
                 height: 56px;
-                margin-right: 16px;
+                display: inline;
+                vertical-align: middle;
+                position: absolute;
+                top: -15px;
             }
+        
             .content {
-                flex: 1;
-                flex-basis: 0.000000001px;
-                display: flex;
                 flex-direction: column;
-                height: 100%;
-                box-sizing: border-box;
                 font-family: var(--font-body);
                 font-size: 16px;
                 color: #22272D;
                 min-width: 200px;
-                display: inline-block;
+                padding: 16px;
             }
-            .actions{
-                display: flex;
-                flex-direction: row;
-                align-items: center;
-            }
+
             .actions::slotted(*) {
                 margin-top: 4px;
+            }
+            .title {
+                color: #3e4042;
+                opacity: 0.4;
+                font-weight: bold;
+                display: inline;
             }
             .markdown-html p {
                 line-height: 28px;
@@ -85,25 +71,39 @@ export class KCEditorBanner extends LitElement {
             [hidden] {
                 display: none !important;
             }
-            .emoji {
-                max-width: 18px;
-                max-height: 18px;
-                transform: translateY(4px);
+            .block {
+                border-bottom: 1px solid rgb(62, 64, 66, 0.3);
+                padding: 10px 0 10px 16px;
+                font-family: var(--font-body);
+                font-size: 16px;
             }
+            .remix {
+                border-top: 1px solid rgb(62, 64, 66, 0.3);
+            }
+
         `, challengeStyles];
     }
     render() {
         return html`
         ${templateContent(button)}
-        <slot name="progress">
-            <kc-circle-progress .value=${this.progress}></kc-circle-progress>
-        </slot>
+        <div class="block block-1">
+            <slot name="block-button"></slot>
+            <slot name="avatar"></slot>
+            <slot name="title" class="title"></slot>
+        </div>
         <div class="content">
+            <slot name="avatar"></slot>
             <div class="markdown-html" id="markdown-html">
                 ${marked(this.text)}
             </div>
             <slot name="actions" class="actions"></slot>
         </div>
+        <!-- @TODO are we supporting remix for first release? should they have their own banner given markup design differ? -->
+        <!-- <div class="block remix">
+            <slot name="title-remix" class="title-remix"></slot>
+            <slot name="tips" class="tips"></slot>
+            <slot name="block-remix-actions" class="actions"></slot>
+        </div> -->
 `;
     }
 }
