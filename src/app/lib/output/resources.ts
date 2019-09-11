58432309deeaa92fa279f81f29260356cd4530c2
Radek Pazdera <radek@kano.me>
2019-09-11 11:47:44 +0100

    Linting

--- src/app/lib/output/resources.ts
+++ src/app/lib/output/resources.ts
@@ -3,46 +3,43 @@ import { join } from '../util/path.js';
 import { Sticker } from '../parts/parts/sticker/types.js';
 
 export interface IResource {
-    id : string,
-    label : string,
-    path : string
+    id : string;
+    label : string;
+    path : string;
 }
 
 export interface IResourceCategory {
-    id : string,
-    label : string,
-    resources : { [key:string]: IResource }
+    id : string;
+    label : string;
+    resources : { [key : string]: IResource };
 }
 
 export interface IResourceArrayWithSrc {
-    id : string,
-    label : string,
-    resources : {
-        id: string,
-        src: string,
-    }[]
+    id : string;
+    label : string;
+    resources : { id : string, src : string }[];
 }
 
 export interface IResourceArrayCategory {
-    id : string,
-    label : string,
-    resources : IResource[]
+    id : string;
+    label : string;
+    resources : IResource[];
 }
 
 export interface IResourceInformation {
-    default? : string,
-    categories? : { [key:string]: IResourceCategory },
-    categorisedResource : IResourceArrayWithSrc[],
-    categoryEnum : [string, string][],
-    getUrl : (id: string | Sticker | null) => string,
-    getRandom : () => string,
-    getRandomFrom : (id: string) => string,
-    cacheValue: (id: string | Sticker) => HTMLImageElement | undefined
+    default? : string;
+    categories? : { [key : string] : IResourceCategory };
+    categorisedResource : IResourceArrayWithSrc[];
+    categoryEnum : [string, string][];
+    getUrl : (id : string | Sticker | null) => string;
+    getRandom : () => string;
+    getRandomFrom : (id : string) => string;
+    cacheValue: (id : string | Sticker) => HTMLImageElement | undefined;
 }
 
 export interface IResources {
-    stickers? : IResourceInformation,
-    get: (id: string) => IResourceInformation | undefined
+    stickers? : IResourceInformation;
+    get: (id: string) => IResourceInformation | undefined;
 }
 
 export class Resource<T> implements IResourceInformation {
@@ -50,7 +47,7 @@ export class Resource<T> implements IResourceInformation {
     categories : { [key : string]: IResourceCategory };
     all: IResource[];
     allCategorised: IResourceArrayWithSrc[];
-    cache = new Map<string | Sticker, HTMLImageElement | undefined>()
+    cache = new Map<string | Sticker, HTMLImageElement | undefined>();
 
     constructor() {
         this.default = '';
@@ -60,22 +57,22 @@ export class Resource<T> implements IResourceInformation {
     }
     addCategory(id : string, label : string) {
         if (this.categories[id]) {
-            console.warn('Category already exists')
-            return
+            console.warn('Category already exists');
+            return;
         }
         this.categories[id] = { id, label, resources: {} };
     }
     add(category : string, id : string, label : string, path : string) {
         if (!this.categories[category]) {
-            console.warn('Category does not exist')
-            return
+            console.warn('Category does not exist');
+            return;
         }
         if (this.categories[category].resources[id]) {
-            console.warn('A sticker with this name has already been created')
-            return
+            console.warn('A sticker with this name has already been created');
+            return;
         }
 
-        this.categories[category].resources[id] = { id, label, path: path }  
+        this.categories[category].resources[id] = { id, label, path: path };
     }
     setDefault(id : string) {
         this.default = id;
@@ -90,56 +87,55 @@ export class Resource<T> implements IResourceInformation {
 
     get resourceSet() {
         if (this.all.length > 0) {
-            return this.all
+            return this.all;
         }
-        
+
         Object.keys(this.categories).forEach(category => {
             Object.keys(this.categories[category].resources).forEach(sticker => {
-                this.all.push(this.categories[category].resources[sticker])
-            })
-        })
+                this.all.push(this.categories[category].resources[sticker]);
+            });
+        });
         return this.all;
     }
 
     get categorisedResource() {
         if (this.allCategorised.length > 0) {
-            return this.allCategorised
+            return this.allCategorised;
         }
 
         Object.keys(this.categories).forEach(category => {
             let categoryObject : IResourceArrayWithSrc = {
                 id: this.categories[category].id,
                 label: this.categories[category].label,
-                resources: []
-            }
-            Object.keys(this.categories[category].resources).forEach(
-                sticker => {
-                    const stickerObject = {id: sticker, src: this.getUrl(sticker)}
-                    categoryObject.resources.push( stickerObject )}
-            )
-            this.allCategorised.push(categoryObject)
-        })
+                resources: [],
+            };
+            Object.keys(this.categories[category].resources).forEach(sticker => {
+                const stickerObject = {id: sticker, src: this.getUrl(sticker)};
+                categoryObject.resources.push( stickerObject );
+            });
+            this.allCategorised.push(categoryObject);
+        });
         return this.allCategorised;
     }
 
     get categoryEnum() {
-        return Object.keys(this.categories).map<[string, string]>(category => [this.categories[category].label, this.categories[category].id])
+        return Object.keys(this.categories).map<[string, string]>(category => [this.categories[category].label, this.categories[category].id]);
     }
 
     cacheValue(id: string | Sticker, type = 'image') {
         if (type !== 'image') {
-            console.warn('cached item type not recognised')
-            return
+            console.warn('cached item type not recognised');
+            return;
         }
 
         if (this.cache.get(id)) {
-            return this.cache.get(id)
+            return this.cache.get(id);
         }
 
-        const newImage = this.syncLoadImg(this.getUrl(id))
+        const newImage = this.syncLoadImg(this.getUrl(id));
 
         this.cache.set(id, newImage);
-        return newImage
+        return newImage;
     }
 
     syncLoadImg(src : string, timeout = 50) {
@@ -174,12 +170,12 @@ export class Resource<T> implements IResourceInformation {
             console.warn(`No sticker provided`);
             return '';
         }
-        let selectedSticker = this.resourceSet.find(sticker => sticker.id === id)
+        let selectedSticker = this.resourceSet.find(sticker => sticker.id === id);
         if (!selectedSticker) {
-            selectedSticker = this.resourceSet.find(sticker => sticker.id === this.default)
+            selectedSticker = this.resourceSet.find(sticker => sticker.id === this.default);
         }
         if (!selectedSticker) {
-            console.warn(`Sticker ${id} does not exist`)
+            console.warn(`Sticker ${id} does not exist`);
             return '';
         }
         return this.resolve(selectedSticker.path);
@@ -188,7 +184,7 @@ export class Resource<T> implements IResourceInformation {
     getLabel(id: string) {
         const selectedSticker = this.resourceSet.find(sticker => sticker.id === id)
         if (!selectedSticker) {
-            console.warn('Sticker does not exist')
+            console.warn('Sticker does not exist');
             return
         }
         return this.resolve(selectedSticker.label);
@@ -196,7 +192,8 @@ export class Resource<T> implements IResourceInformation {
 }
 
 export class Resources implements IResources {
-    resources: Map <string, IResourceInformation>
+    resources: Map <string, IResourceInformation>;
+
     constructor() {
         this.resources = new Map();
         this.resources.set('stickers', new Resource<HTMLImageElement>());
