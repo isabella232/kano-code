b0e236b056a8f2da506ba448a463ac85bcc7d6bf
Paul McKenna <paulmck21@gmail.com>
2019-11-20 15:58:24 +0000

    fix: old shares apostrophe fix

--- src/app/lib/editor/editor-or-player.ts
+++ src/app/lib/editor/editor-or-player.ts
@@ -6,10 +6,22 @@ export abstract class EditorOrPlayer extends PluginReceiver {
     abstract output : Output;
     abstract inject(host : HTMLElement, before? : HTMLElement) : void;
     replaceSource(app : any) {
-        if (app && app.source) {
+        if (app && app.code && app.code.snapshot) {
+            if (app.code.snapshot
+                && app.code.snapshot.javascript
+                && app.code.snapshot.javascript.replace) {
+                app.code.snapshot.javascript = app.code.snapshot.javascript.replace(/(&rsquo;)/g, '’');
+            }
+            if (app.code.snapshot
+                && app.code.snapshot.blocks
+                && app.code.snapshot.blocks.replace) {
+                app.code.snapshot.blocks = app.code.snapshot.blocks.replace(/(&rsquo;)/g, '’');
+            }
+        }
+        if (app && app.source && app.source.replace) {
             app.source = app.source.replace(/(&rsquo;)/g, '’');
         }
-        if (app && app.code) {
+        if (app && app.code && app.code.replace) {
             app.code = app.code.replace(/(&rsquo;)/g, '’');
         }
         return app;
